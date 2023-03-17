14c7182215cf71434af66c1ceeeb263955eec7b7
Piotr Drozdek <piotrdrozdek24@gmail.com>
2023-03-17 11:39:07 +0100

    Prosty kalkulator

--- NaukaJava/Main.java
+++ NaukaJava/Main.java
@@ -1,6 +1,20 @@
+import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello" );
+    public static void main(String[]args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Czesc podaj 1 liczbe");
+        int firstNumber = scanner.nextInt();
+        System.out.println("Podaj 2 liczbe");
+        int secoundNumber = scanner.nextInt();
+        int dodaj, odejmij, podziel, pomnoz;
+        dodaj = firstNumber+secoundNumber;
+        odejmij = firstNumber-secoundNumber;
+        podziel = firstNumber/secoundNumber;
+        pomnoz = firstNumber*secoundNumber;
+        System.out.println("Ich suma to " + dodaj);
+        System.out.println("Ich odejmowanie to " + odejmij);
+        System.out.println("Ich dzieleni to " + podziel);
+        System.out.println("Ich mnożenie to " + pomnoz);
     }
 }
