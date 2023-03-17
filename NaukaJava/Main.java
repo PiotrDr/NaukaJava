08e73eb6cea4ff9f471381efba3e611c58613c76
Piotr Drozdek <piotrdrozdek24@gmail.com>
2023-03-17 10:51:14 +0100

    test zmiany formatu danych

--- NaukaJava/Main.java
+++ NaukaJava/Main.java
@@ -1,6 +1,9 @@
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello" );
+        int jeden = 2;
+        int dwa = 3;
+        Integer dziel = jeden/dwa;
+        System.out.println(dziel.doubleValue() );
     }
 }
