bc162b051c021e9afd01ebbd81036b7a1b9b57f3
Piotr Drozdek <piotrdrozdek24@gmail.com>
2023-05-04 20:39:06 +0200

    Zadania tworzenie klas i obiektow klas

--- NaukaJava/StudentCheck.java
+++ NaukaJava/StudentCheck.java
@@ -8,23 +8,28 @@ public class StudentCheck {
         nr1.email = "adam@mail.com";
         nr1.nrIndeksu = 12345;
         nr1.nick = "AdamxD1";
+
         nr2.imie = "Ania";
         nr2.nazwisko = "Nowak";
         nr2.email = "ania@mail.com";
         nr2.nrIndeksu = 23456;
         nr2.nick = "AnnaPanna";
+
         nr3.imie = "Kasia";
         nr3.nazwisko = "Nowa";
         nr3.email = "kasia@mail.com";
         nr3.nrIndeksu = 34567;
         nr3.nick = "Kacha";
 
-        Student[] tablica = new Student[] {nr1, nr2, nr3};
-        for (int i = 0; i < tablica.length; i++){
-            tablica[i].przedstawSie();
-            tablica[i].podajEmail();
-            tablica[i].podajNrIndeksu();
-            tablica[i].zalogujSie();
+        Student[] students = new Student[3];
+        students[0] = nr1;
+        students[1] = nr2;
+        students[2] = nr3;
+        for (int i = 0; i < students.length; i++){
+            students[i].przedstawSie();
+            students[i].podajEmail();
+            students[i].podajNrIndeksu();
+            students[i].zalogujSie();
         }
 
     }
