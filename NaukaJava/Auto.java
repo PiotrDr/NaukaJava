e765f534ac3ec8a5e9858a60d79481f1adb4c89e
Piotr Drozdek <piotrdrozdek24@gmail.com>
2023-05-09 17:37:20 +0200

    Uzupełnienie o wymagane parametry

--- NaukaJava/Auto.java
+++ NaukaJava/Auto.java
@@ -4,6 +4,12 @@ public class Auto {
     public int rokProdukcji;
     public int przebieg;
 
+    public Auto(String marka, String model, int rokProdukcji, int przebieg){
+        this.marka = marka;
+        this.model = model;
+        this.rokProdukcji = rokProdukcji;
+        this.przebieg = przebieg;
+    }
     public void jedz() {
         System.out.println("Jedz!");
     }
